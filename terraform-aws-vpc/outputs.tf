e93c33c790794e4bb83f374f31178eea13e3596b
Vijay Guntireddy <vijay.guntireddy@gmail.com>
2024-09-27 12:01:17 +0530

    uploading picture of VPC project

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -13,3 +13,15 @@ output "default_vpc_info" {
 #  output "main_route_table_info" {
 #   value = data.aws_route_table.main
 # }
+
+output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
--- vpc-module-test/outputs.tf
+++ vpc-module-test/outputs.tf
@@ -5,10 +5,14 @@ output "vpc_id" {
 #     value = module.vpc.az_info
 # }
 
-output "default_vpc_info"{
-    value = module.vpc.default_vpc_info
-}
+# output "default_vpc_info"{
+#     value = module.vpc.default_vpc_info
+# }
 
 # output "main_route_table_info" {
 #      value = module.vpc.main_route_table_info
 #  }
+
+output "public_subnet_ids" {
+    value = module.vpc.public_subnet_ids
+}
