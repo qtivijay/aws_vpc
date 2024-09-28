2de19016e8166ae696b2b3f8a6ec951e4205d962
Vijay Guntireddy <vijay.guntireddy@gmail.com>
2024-09-28 16:44:41 +0530

    uploading picture of VPC project

--- vpc-module-test/vpc.tf
+++ vpc-module-test/vpc.tf
@@ -1,6 +1,6 @@
 module vpc{
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/qtivijay/aws_vpc.git?ref=main"
+    source = "https://github.com/qtivijay/aws_vpc/tree/6db09f77e57df68c4a33acf44c0b29e868789c7b/terraform-aws-vpc"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
